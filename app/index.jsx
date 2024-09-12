ac61d2e488c8e8cafb2b848152714b0a7c963c64
manminder singh <bewithme2407@gmail.com>
2024-09-12 21:14:30 +0530

    setup

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className=" h-screen w-full flex items-center justify-center text-center text-white"
+    >
+      <Text className=" w-56 text-lg text-pink-600 font-bold" >Welcome shreya .. you can code now . for any query contact manminder.</Text>
+    </View>
+  );
+}
